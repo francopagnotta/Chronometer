d48302831678bdcbaeaec7f4657d70b2d7176a63
FrancoPagnotta <francoapagnotta@gmail.com>
2021-07-22 22:43:47 -0300

    color change to play title

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function startStop() {
 		
 		// title change
 			document.querySelector('.title').innerHTML = 'PLAY';
-			document.querySelector('.title').style.color = '#7bf1a8'
+			document.querySelector('.title').style.color = '#02DA17'
 	} 
 	
 	else {
